848ea64ee215fc4a8b7c5ce098710b502cc3644d
Alexander Tømmerås <flugged@gmail.com>
2020-01-15 09:31:06 +0100

    fix(git): remove undefined function call

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,5 @@
 message=$(tput bold)$(tput setaf 6)
+question=`tput bold`
 success=`tput setaf 2`
 info=`tput setaf 3`
 reset=`tput sgr0`
@@ -11,9 +12,9 @@ if [[ ! -e $HOME/.gitconfig ]]; then
   ln -s $HOME/.dotfiles/git/.gitignore $HOME/.gitignore
   echo "${success}Linked ${HOME}/.dotfiles/git/.gitignore to ${HOME}/gitignore.${reset}"
 
-  question 'What is your name?'
+  echo "${question}What is your name?${reset}"
   read -r user_name
-  question 'What is your email address?'
+  echo "${question}What is your email address?${reset}"
   read -r user_email
 
   git config --global user.name "${user_name}"
