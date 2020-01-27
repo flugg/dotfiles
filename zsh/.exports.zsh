bd76f8a25d4f495285966ddfb920d835e2fa5b40
Alexander Tømmerås <flugged@gmail.com>
2020-01-27 16:04:45 +0100

    feature(zsh): add locale export

--- zsh/.exports.zsh
+++ zsh/.exports.zsh
@@ -10,3 +10,6 @@ export XDEBUG_CONFIG="idekey=VSCODE"
 
 # Lazy load NVM to improve shell startup time
 export NVM_LAZY_LOAD=true
+
+# Set locale to UTF to fix some warnings
+export LC_ALL=en_US.UTF-8
